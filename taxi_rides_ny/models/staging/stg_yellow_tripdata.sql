0cc33f98231ea4719a68a8378268d8f3a6fd60d0
cj low <keitai5263@hotmail.com>
2025-08-29 06:42:07 +0000

    unfinished

--- taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -10,10 +10,10 @@ with tripdata as
 select
    -- identifiers
     {{ dbt_utils.generate_surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,    
-    {{ dbt.safe_cast("vendorid", api.Column.translate_type("integer")) }} as vendorid,
-    {{ dbt.safe_cast("ratecodeid", api.Column.translate_type("integer")) }} as ratecodeid,
-    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
-    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    {{ dbt.safe_cast("VendorID", api.Column.translate_type("integer")) }} as vendorid,
+    {{ dbt.safe_cast("RatecodeID", api.Column.translate_type("integer")) }} as ratecodeid,
+    {{ dbt.safe_cast("PULocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("DOLocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
 
     -- timestamps
     cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
@@ -32,8 +32,8 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(0 as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(congestion_surcharge as numeric) as congestion_surcharge,
     cast(total_amount as numeric) as total_amount,
     coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description
